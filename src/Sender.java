7c31bbc603f36617d4c0898faceecbe4748705df
PatrickGharib <36083375+PatrickGharib@users.noreply.github.com>
2019-03-22 10:51:20 -0600

    main method, sendToSerializer method
    main: 
    initialize host and port
    enter object creator
    create XML
    
    sendToSerializer: 
    serialize each object and create the file
    prep to send file to the reciever

--- src/Sender.java
+++ src/Sender.java
@@ -1,2 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import org.jdom2.*;
+import org. jdom2.output.*;
+import java.net.*;
+
 public class Sender {
+
+    public static void main(String[] args){
+        String host = "localhost";
+        int port = 8000;
+        ArrayList<Object> objectsToSerialize = ObjectCreator.userCreationPrompt();
+        sendToSerializer(port, host, objectsToSerialize);
+
+    }
+    private static void sendToSerializer(int port, String host, ArrayList<Object> objectToSerialize){
+        for (Object object: objectToSerialize){
+            Class classObject = object.getClass();
+            System.out.println("Serializing object " +
+                    classObject.getName() + "...");
+            //TODO make method in Serializer to serialize the object
+            Document document = Serializer.serializeObject(object);
+            File f = new File("FileSent.xml");
+            try {
+                XMLOutputter xmlOutputter = new XMLOutputter();
+                xmlOutputter.setFormat(Format.getPrettyFormat());
+                FileWriter fw = new FileWriter(f);
+                BufferedWriter bw = new BufferedWriter(fw);
+                xmlOutputter.output(document, bw);
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
