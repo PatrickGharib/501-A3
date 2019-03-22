6f5755a8a966e2777012e3e6c588f0d7eb2470c9
PatrickGharib <36083375+PatrickGharib@users.noreply.github.com>
2019-03-21 22:38:23 -0600

    added method to create object containing array
    prompts user for length of array
    prompts user for value for each integer
    creates object containing the primitive array that was created by user

--- src/ObjectCreator.java
+++ src/ObjectCreator.java
@@ -6,7 +6,8 @@ public class ObjectCreator{
 
 
         }
-    public static userCreationPrompt(){
+        //TODO give this a return type
+    private static void userCreationPrompt(){
         System.out.println("Please Proceed in creating the objects you wish to send: \n" +
                 "1) Simple object with primitive fields\n" +
                 "2) Object containing references to other objects\n" +
@@ -55,18 +56,45 @@ public class ObjectCreator{
             System.out.println("Please enter either \"true\" or \"false\"!");
         }
         boolean booleanField = userin.nextBoolean();
-        System.out.println("SimpleObject Creation in progress...")
+        System.out.println("SimpleObject Creation in progress...");
         SimpleObject newSimpleObject = new SimpleObject(intField, booleanField);
-        System.out.println("Done...")
+        System.out.println("Done...");
         return newSimpleObject;
     }
     private static ReferenceObject createReferenceObject(){
-       System.out.println("Reference object has one field: " +
+       System.out.println("ReferenceObject has one field: " +
                "\n\t- SimpleObjectField");
        SimpleObject newSimpleObject = createSimpleObject();
        System.out.println("Creation of object with reference to an object now in progress...");
        ReferenceObject newReferenceObject = new ReferenceObject(newSimpleObject);
+       System.out.println("done...");
        return newReferenceObject;
     }
-    
+    private static PrimArrayObject createPrimitiveArrayObject(){
+        System.out.println("PimArrayObject has one field:" +
+                "\n\t- intArray");
+        Scanner userin = new Scanner(System.in);
+        int arrayLength;
+        System.out.println("Please enter the number of integers you would like in your array: ");
+
+        while (!userin.hasNextInt()){
+            userin.next();
+            System.out.print("Please enter a valid Integer");
+        }
+        arrayLength = userin.nextInt();
+        int[] primitiveArray = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++){
+            System.out.println("Please specify a number to insert at index " +
+                    primitiveArray[i] + ":" );
+            while(!userin.hasNextInt()){
+                userin.next();
+                System.out.println("Please enter a valid Integer");
+            }
+            primitiveArray[i] = userin.nextInt();
+        }
+        System.out.println("creation of object with primitive array in progress...");
+        PrimArrayObject newPrimArrayObject = new PrimArrayObject(primitiveArray);
+        System.out.println("done...");
+        return newPrimArrayObject;
+    }
 }
